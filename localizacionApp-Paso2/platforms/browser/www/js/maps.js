a5c89af39b6dac9cdd609942178971a15a5df5f9
JaimeMongay <JaimeMongay@users.noreply.github.com>
2017-12-11 18:50:52 +0100

    App Pasos
    Finalización de los pasos de la app localizacion

---
+++ localizacionApp-Paso2/platforms/browser/www/js/maps.js
@@ -0,0 +1,37 @@
+var app = {
+  inicio: function() {
+    this.iniciaFastClick();
+
+  },
+
+  iniciaFastClick:function () {
+    FastClick.attach(document.body);
+  },
+
+  dispositivoListo: function(){
+    navigator.geolocation.getCurrentPosition(app.pintaCoordenadasEnMapa, app.errorAlSolicitarLocalizacion);
+  },
+
+  pintaCoordenadasEnMapa: function(position){
+    var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFpbWVtIiwiYSI6ImNqYXdxZzNuYzdsYTkyd2p1a2s2NzhlN2UifQ.x1fZdfJh6vCqYbeuVbKJ4A', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(miMapa);
+  },
+
+  errorAlSolicitarLocalizacion: function(error){
+    console.log(error.code + ': ' + error.message);
+  }
+
+};
+
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    app.inicio();
+  }, false);
+  document.addEventListener('deviceready', function() {
+    app.dispositivoListo();
+  }, false);
+}
